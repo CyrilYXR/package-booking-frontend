623a7361780303740c8e3e038c535b6b6357f75e
CyrilYao <cyril.yao@oocl.com>
2019-07-25 11:53:22 +0800

    add head and buttons

---
+++ src/components/package-list.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <ol>
+      <!-- <li v-for="package in packagelist" :key="package.id"> -->
+
+      <!-- </li> -->
+    </ol>
+  </div>
+</template>
+<script>
+export default {
+  name: "package-list",
+  components: {},
+  computed: {
+    packagelist() {
+      return this.$store.state.packagelist;
+    }
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,43 @@
 <template>
   <div class="home">
-    
-    
+    <div>
+      <a-row>
+        <a-col :span="9">菜鸟驿站</a-col>
+        <a-col :span="1">
+          <a-button>All</a-button>
+        </a-col>
+        <a-col :span="1">
+          <a-button>已预约</a-button>
+        </a-col>
+        <a-col :span="1">
+          <a-button>已取件</a-button>
+        </a-col>
+        <a-col :span="1">
+          <a-button>未预约</a-button>
+        </a-col>
+        <a-col :span="1">
+          <a-button>+添加</a-button>
+        </a-col>
+      </a-row>
+    </div>
+    <div>
+      <packagelist></packagelist>
+    </div>
   </div>
 </template>
 
 <script>
-
+import packagelist from '../components/package-list.vue'
 export default {
-  name: 'home',
+  name: "home",
   components: {
+    packagelist
+  },
+  methods(){
     
+  },
+  created: {
+
   }
-}
+};
 </script>
