497ff6066112148ae4a964a97d2537140654f59d
Cyril <1075887346@qq.com>
2019-07-27 00:15:24 +0800

    complete add new package

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,20 +1,34 @@
 import axios from 'axios'
 
 export default {
-    loadPackageList({commit}){
+    loadPackageList({ commit }) {
         axios.get('http://localhost:8084/packages')
-        .then(function(response){
-            commit('loadPackageList', response.data)
-        }).catch(function (error){
-            console.log(error)
-        })
+            .then(function (response) {
+                commit('loadPackageList', response.data)
+            }).catch(function (error) {
+                console.log(error)
+            })
     },
-    filterByStatus({commit}, status){
+    filterByStatus({ commit }, status) {
         axios.get('http://localhost:8084/packages?status=' + status)
-        .then(function(response){
-            commit('loadPackageList', response.data)
-        }).catch(function (error){
+            .then(function (response) {
+                commit('loadPackageList', response.data)
+            }).catch(function (error) {
+                console.log(error)
+            })
+    },
+    addPackage({ commit }, addEntity) {
+        addEntity.status = 0;
+        axios.post('http://localhost:8084/packages', addEntity)
+            .then(function (response) {
+                axios.get('http://localhost:8084/packages')
+                .then(function (response) {
+                    commit('loadPackageList', response.data)
+                }).catch(function (error) {
+                    console.log(error)
+                })
+        }).catch(function (error) {
             console.log(error)
         })
-    }
+}
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,52 +16,126 @@
           <a-button @click="filterByStatus(0)">未预约</a-button>
         </a-col>
         <a-col :span="2">
-          <a-button>+添加</a-button> 
+          <a-button type="primary" @click="showModal">+添加</a-button>
+          <a-modal
+            :visible="visible"
+            title="Create a new collection"
+            okText="Create"
+            @cancel="handleCancel()"
+            @ok="handleCreate()"
+          >
+            <a-form layout="vertical" :form="form">
+              <a-form-item label="运单号" v-model="addEntity.id">
+                <a-input
+                  v-decorator="[
+              'id',
+              { rules: [{ required: true, message: 'Please input the content!' }],}
+            ]"
+                />
+              </a-form-item>
+
+              <a-form-item label="收件人" v-model="addEntity.name">
+                <a-input
+                  v-decorator="[
+              'name',
+              { rules: [{ required: true, message: 'Please input the content!' }],}
+            ]"
+                />
+              </a-form-item>
+
+              <a-form-item label="电话" v-model="addEntity.phone">
+                <a-input
+                  v-decorator="[
+              'phone',
+              { rules: [{ required: true, message: 'Please input the content!' }],}
+            ]"
+                />
+              </a-form-item>
+              
+              <a-form-item label="重量" v-model="addEntity.weight">
+                <a-input
+                  v-decorator="[
+              'weight',
+              { rules: [{ required: true, message: 'Please input the content!' }],}
+            ]"
+                />
+              </a-form-item>
+            </a-form>
+          </a-modal>
         </a-col>
       </a-row>
     </div>
     <div>
       <table>
-      <tr>
-        <td>运单号</td>
-        <td>收件人</td>
-        <td>电话</td>
-        <td>状态</td>
-        <td>预约时间</td>
-        <td>操作</td>
-      </tr>
-      <tr v-for="packageData in packages" :key="packageData.id">
-        <td>{{packageData.id}}</td>
-        <td>{{packageData.name}}</td>
-        <td>{{packageData.phone}}</td>
-        <td>{{packageData.status}}</td>
-        <td>{{packageData.reserveTime}}</td>
-      </tr>  
-    </table>
+        <tr>
+          <td>运单号</td>
+          <td>收件人</td>
+          <td>电话</td>
+          <td>状态</td>
+          <td>预约时间</td>
+          <td>操作</td>
+        </tr>
+        <tr v-for="packageData in packages" :key="packageData.id">
+          <td>{{packageData.id}}</td>
+          <td>{{packageData.name}}</td>
+          <td>{{packageData.phone}}</td>
+          <td>{{showStatus(packageData.status)}}</td>
+          <td>{{packageData.reserveTime}}</td>
+        </tr>
+      </table>
     </div>
   </div>
 </template>
 
 <script>
+import { constants } from 'crypto';
 export default {
-  name: 'home',
-  components: {
-  },
+  name: "home",
+  components: {},
   created() {
-    this.$store.dispatch('loadPackageList');
+    this.$store.dispatch("loadPackageList");
   },
-  computed:{
-    packages(){
-      return this.$store.state.packages
+  computed: {
+    packages() {
+      return this.$store.state.packages;
     }
   },
-  methods:{
-    loadAllPackages(){
-      this.$store.dispatch('loadPackageList');
+  methods: {
+    loadAllPackages() {
+      this.$store.dispatch("loadPackageList");
     },
     filterByStatus(status) {
-      this.$store.dispatch('filterByStatus', status)
+      this.$store.dispatch("filterByStatus", status);
+    },
+    showModal () {
+      this.visible = true;
+    },
+    handleCancel  () {
+      this.visible = false;
+    },
+    handleCreate () {
+      const form = this.form
+      form.validateFields((err, values) => {
+        if (err) {
+          console.log(err)
+          return;
+        }
+        console.log('Received values of form: ', values);
+        this.$store.dispatch('addPackage',values);
+        form.resetFields();
+        this.visible = false;
+      });
+    },
+    showStatus(status){
+      return status === 0 ? "未取件" : status === 1 ? "已预约" : "已取件"
+    }
+  },
+  data(){
+    return{
+      visible: false,
+      form: this.$form.createForm(this),
+      addEntity: {'id':'','name':'','weight':'','phone':'','stauts':0},
     }
   }
-}
+};
 </script>
